c44b66b41fed1ebb63ae017e97b5c7a5dd6b07a7
kfiva <kfivap@gnail.com>
2021-02-15 12:33:22 +0300

    initial

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useContext} from 'react'
+import {NavLink, useHistory} from 'react-router-dom'
+import {AuthContext} from "../context/AuthContext";
+
+export const Navbar = () =>{
+    const history = useHistory()
+
+    const auth = useContext(AuthContext)
+
+    const logoutHandler = event =>{
+        event.preventDefault()
+        auth.logout()
+        history.push('/')
+    }
+
+    return(
+        <nav>
+            <div className="nav-wrapper blue darken-1"
+            style={{padding: '0 2rem'}}
+            >
+                <span className="brand-logo right">Сокращение ссылок</span>
+                <ul  >
+                    <li><NavLink to={'/create'}>Create</NavLink></li>
+                    <li><NavLink to={'/links'}>Links</NavLink></li>
+                    <li><a href='/' onClick={logoutHandler}>Выйти</a></li>
+
+                </ul>
+            </div>
+        </nav>)
+}
---
+++ routes/auth.routes.js
@@ -0,0 +1,104 @@
+const {Router} = require('express')
+const bcrypt = require('bcrypt')
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const {check, validationResult} = require('express-validator')
+const User = require('../models/User')
+const router =  Router()
+
+// /api/auth/register
+router.post(
+    '/register',
+    [
+        check('email', 'incorrect email').isEmail(),
+        check('password', 'min length 6 symbols').isLength({min:6})
+    ],
+    async (req, res) =>{
+    try{
+        console.log('body: ', req.body)
+        const errors = validationResult(req)
+
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: 'incorrect registration data'
+            })
+        }
+
+        const {email, password} = req.body
+
+        const candidate = await User.findOne({email: email})
+
+        if(candidate){
+            return res.status(400).json({message: 'this user already exist'})
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const user = new User({email, password: hashedPassword})
+
+        await user.save()
+
+        res.status(201).json({message: 'user added'})
+
+
+
+} catch (e) {
+        res.status(500).json({ message: "something went wrong"})
+        console.log(e)
+
+}
+})
+
+
+// /api/auth/login
+router.post('/login',
+    [
+        check('email', 'type correct email').normalizeEmail().isEmail(),
+        check('password', 'type password').exists()
+    ],
+    async (req, res) =>{
+        try{
+
+
+            const errors = validationResult(req)
+
+
+            if(!errors.isEmpty()){
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'incorrect login data'
+                })
+            }
+
+            const {email, password} = req.body
+
+            const user = await User.findOne({ email})
+
+            if (!user){
+                return res.status(400).json({message: 'user not found'})
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password)
+
+            if (!isMatch){
+                return res.status(400).json({
+                    message: 'incorrect password try again'
+                })
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecret'),
+                {expiresIn: '1h'}
+            )
+
+            res.json({token, userId: user.id})
+
+        } catch (e) {
+            res.status(500).json({ message: "something went wrong"})
+           console.log(e)
+        }
+})
+
+
+module.exports  =router
